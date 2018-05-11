b4f9e5fe2984b851f1f08f075014ea571a3c7584
yuri <yuri.gouveia@eng.ci.ufpb.br>
2018-05-11 15:43:14 -0300

    Colocar codigo do Esp aqui

---
+++ EspWifi/safeband.ino
@@ -0,0 +1,1 @@
+/*SOFTWARE PARA SISTEMAS EMBARCADOSYURI GOUVEIAWELLTON THYAGOGEORGE NUNESPROF. ALISSON BRITOCódigo do esp8266, que realiza a comunicação entre o botãoexterno com o aplicativo por meio firebase.*/void setup() {    }void loop() {    }
